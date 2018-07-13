366219a17fa3da94d75be7396811e728c85dd9cd
Emeka Ezike <franklinezike@gmail.com>
2018-07-12 20:43:48 -0500

    Collected and passed data in segue

---
+++ 'Za Hunter/LocationDetailsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LocationDetailsViewController.swift
+//  'Za Hunter
+//
+//  Created by Emeka Ezike on 7/12/18.
+//  Copyright © 2018 Emeka Ezike. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    
+}
